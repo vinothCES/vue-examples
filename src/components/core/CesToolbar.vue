a98415a7cc3201f34ab99893e2e713cc144111b2
Vinoth J <vinoth.jayaprakash@cesltd.com>
2019-03-19 20:11:23 +0530

    Created a toolbar component

---
+++ src/components/core/CesToolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <v-toolbar app flat class="grey lighten-4">
+      <v-toolbar-title class="grey--text text--darken-3">
+        <v-img src="images/CES_logo.png" height="36" width="36" alt="CES Logo">
+        </v-img>
+      </v-toolbar-title>
+      <v-toolbar-title class="grey--text text--darken-3 mr-5">
+        <span class="mr-2">Vuex</span>
+        <span class="font-weight-light">Example</span>
+      </v-toolbar-title>
+    </v-toolbar>
+  </nav>
+</template>
